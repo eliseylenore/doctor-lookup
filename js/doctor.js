f2f9b5d2bc2af7d530b03fc6c0e32d8dc58c1765
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 12:28:30 -0700

    Add condition for displaying website

--- js/doctor.js
+++ js/doctor.js
@@ -7,6 +7,7 @@ Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=2&user_key=' + apiKey)
   .then(function(response) {
     var rating;
+    var website = response.data[0].practices[0].website;
     if (response.data[0].ratings.length > 0) {
       if (response.data[0].ratings[0] !== undefined) {
         if(response.data[0].ratings[0].image_url_small !== undefined) {
@@ -20,6 +21,10 @@ Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
     else {
       rating = "./img/no-rating.png";
     }
+
+    if(website === undefined) {
+      website = "";
+    }
     displayDoctor(
       response.data[0].profile.image_url,
       response.data[0].profile.first_name,
@@ -30,7 +35,7 @@ Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
       response.data[0].practices[0].visit_address.state,
       response.data[0].practices[0].visit_address.zip,
       response.data[0].practices[0].phones[0].number,
-      response.data[0].practices[0].website);
+      website);
       console.log(rating);
    })
   .fail(function(error){
