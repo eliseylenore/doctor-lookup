1ab5b4b7f5928261ac806b87a03ba38dedbca95d
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 16:10:26 -0700

    Increase font size for doctor name

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,7 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(image, first, last, rating, street, city, state, zip, phone, website) {
-  $('.results').append('<div class="row profile"><div class="col-xs-5 col-sm-3 col-md-2"><img class="img-circle" src="' + image + '" alt="' + first + " " + last + '" /></div><div class="col-xs-7 col-sm-9 col-md-10"' + '<h5>' + first + " " + last + '<img class="rating" src="' + rating + '" alt="rating"/></h5><h7>' + street + '</h7>' + '<h7>' + city + ', ' + state + ' ' + zip + '</h7><h4>' + phone + '</h4><h8><a href="' + website + '">' + website + '</a></h8></div></div>');
+  $('.results').append('<div class="row profile"><div class="col-xs-5 col-sm-3 col-md-2"><img class="img-circle" src="' + image + '" alt="' + first + " " + last + '" /></div><div class="col-xs-7 col-sm-9 col-md-10">' + '<h3>' + first + " " + last + '</h3><img class="rating" src="' + rating + '" alt="rating"/><h7>' + street + '</h7>' + '<h7>' + city + ', ' + state + ' ' + zip + '</h7><h8>' + phone + '</h8><h8><a href="' + website + '">' + website + '</a></h8></div></div>');
 };
 
 $(document).ready(function() {
