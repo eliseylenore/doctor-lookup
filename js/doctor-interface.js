7bc594e17d48462e362c32edebd6cc6101118ed0
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 15:07:15 -0700

    Adjust spacing and sizing of results

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,7 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(image, first, last, rating, street, city, state, zip, phone, website) {
-  $('.results').append('<img class="img-circle" src="' + image + '" alt="' + first + " " + last + '" />' + '<h3>' + first + " " + last + '</h3><img class="rating" src="' + rating + '" alt="rating" /><h5>' + street + ', ' + city + ', ' + state + ' ' + zip + '</h5><h4>' + phone + '</h4><h4><a href="' + website + '">' + website + '</a></h4>');
+  $('.results').append('<img class="img-circle center-block" src="' + image + '" alt="' + first + " " + last + '" />' + '<h5>' + first + " " + last + '<img class="rating" src="' + rating + '" alt="rating"/></h5><h7>' + street + '</h7>' + '<h7>' + city + ', ' + state + ' ' + zip + '</h7><h4>' + phone + '</h4><h8><a href="' + website + '">' + website + '</a></h8>');
 };
 
 $(document).ready(function() {
