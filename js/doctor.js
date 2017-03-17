ee260ef0f5ca5470ef865f7ea8cbdd4a1427b3b4
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 13:54:47 -0700

    Put form in column

--- js/doctor.js
+++ js/doctor.js
@@ -6,9 +6,11 @@ Doctor = function() {
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=2&user_key=' + apiKey)
   .then(function(response) {
+    //loop through found docotors
     response.data.forEach(function(person) {
       var rating;
       var website = response.data[0].practices[0].website;
+      //Determine whether ratings are listed, and where they are listed
       if (response.data[0].ratings.length > 0) {
         if (response.data[0].ratings[0] !== undefined) {
           if(response.data[0].ratings[0].image_url_small !== undefined) {
@@ -22,7 +24,7 @@ Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
       else {
         rating = "./img/no-rating.png";
       }
-
+      //determine whether website is listed for person
       if(website === undefined) {
         website = "";
       }
