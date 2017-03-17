f34bb4ae1c50066487e6b65752eda5874c22d03e
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 12:15:44 -0700

    Fix formatting of rating image

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,7 +1,7 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
 var displayDoctor = function(image, first, last, rating, street, city, state, zip, phone, website) {
-  $('.results').append('<img class="img-circle" src="' + image + '" alt="' + first + " " + last + '" />' + '<h3>' + first + " " + last + '</h3><img src"' + rating + '"/><h5>' + street + ', ' + city + ', ' + state + ' ' + zip + '</h5><h4>' + phone + '</h4><h4><a href="' + website + '">' + website + '</a></h4>');
+  $('.results').append('<img class="img-circle" src="' + image + '" alt="' + first + " " + last + '" />' + '<h3>' + first + " " + last + '</h3><img class="rating" src="' + rating + '" alt="rating" /><h5>' + street + ', ' + city + ', ' + state + ' ' + zip + '</h5><h4>' + phone + '</h4><h4><a href="' + website + '">' + website + '</a></h4>');
 };
 
 $(document).ready(function() {
--- js/doctor.js
+++ js/doctor.js
@@ -6,30 +6,32 @@ Doctor = function() {
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=2&user_key=' + apiKey)
   .then(function(response) {
-  //   displayDoctor(
-  //     response.data[0].profile.image_url,
-  //     response.data[0].profile.first_name,
-  //     response.data[0].profile.last_name,
-  //     response.data[0].practices[0].visit_address.street,
-  //     response.data[0].practices[0].visit_address.city,
-  //     response.data[0].practices[0].visit_address.state,
-  //     response.data[0].practices[0].visit_address.zip,
-  //     response.data[0].practices[0].phones[0].number,
-  //     response.data[0].practices[0].website);
-  if (response.data[0].ratings.length > 0) {
-    if (response.data[0].ratings[0] !== undefined) {
-      if(response.data[0].ratings[0].image_url_small !== undefined) {
-        console.log(response.data[0].ratings[0].image_url_small);
+    var rating;
+    if (response.data[0].ratings.length > 0) {
+      if (response.data[0].ratings[0] !== undefined) {
+        if(response.data[0].ratings[0].image_url_small !== undefined) {
+          rating = response.data[0].ratings[0].image_url_small;
+        }
+        else {
+          rating = response.data[0].ratings[1].image_url_small;
+        }
       }
-      else {
-        console.log(response.data[0].ratings[1].image_url_small)
-      }
-      console.log("rating is not undefined at ratings[0]");
     }
-  }
-  else {
-    console.log("doesn't have rating");
-  }
+    else {
+      rating = "./img/no-rating.png";
+    }
+    displayDoctor(
+      response.data[0].profile.image_url,
+      response.data[0].profile.first_name,
+      response.data[0].profile.last_name,
+      rating,
+      response.data[0].practices[0].visit_address.street,
+      response.data[0].practices[0].visit_address.city,
+      response.data[0].practices[0].visit_address.state,
+      response.data[0].practices[0].visit_address.zip,
+      response.data[0].practices[0].phones[0].number,
+      response.data[0].practices[0].website);
+      console.log(rating);
    })
   .fail(function(error){
      console.log("fail");
