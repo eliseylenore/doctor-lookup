d03e43c83bf5366aef675ba52a86cb3fa420c790
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 09:36:28 -0700

    Add doctor-interface, return query object

---
+++ js/doctor-interface.js
@@ -0,0 +1,15 @@
+var Doctor = require('./../js/doctor.js').doctorModule;
+
+var displayDoctor = function(image, first, last, street, city, state, zip, phone) {
+  $('.results').append('<img src=""' + image + ' alt="" />' + '<h3>' + first + ' ' + last + '</h3><h5>' + street + ', ' + city + ', ' + state + ' ' + zip + '</h5><h4>' + phone + '</h4>');
+};
+
+$(document).ready(function() {
+  var currentDoctorObject = new Doctor();
+  $('form').submit(function(event) {
+    event.preventDefault();
+    var medicalIssue = $('#medical-issue').val();
+    $('#medical-issue').val('');
+    currentDoctorObject.getDoctor(medicalIssue);
+  });
+});
--- js/doctor.js
+++ js/doctor.js
@@ -1,10 +1,10 @@
 var apiKey = require('./../.env').apiKey;
 
 Doctor = function() {
-}
+};
 
-Doctor.prototype.getDoctor = fucntion(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDoctor = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
   .then(function(result) {
      console.log(result);
    })
@@ -12,3 +12,5 @@ Doctor.prototype.getDoctor = fucntion(medicalIssue) {
      console.log("fail");
    });
 };
+
+exports.doctorModule = Doctor;
