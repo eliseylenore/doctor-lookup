ae50db2c13eef0598ed9d651dc69d04e0136901a
Elise St Hilaire <eliseylenore@gmail.com>
2017-03-17 16:11:34 -0700

    increase size of results to 10

--- js/doctor.js
+++ js/doctor.js
@@ -4,7 +4,7 @@ Doctor = function() {
 };
 
 Doctor.prototype.getDoctor = function(medicalIssue, displayDoctor) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=2&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
   .then(function(response) {
     //loop through found docotors
     response.data.forEach(function(person) {
